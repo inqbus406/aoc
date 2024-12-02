c19148e06582be8a1e63b1299bc6c019a753bc0d
Jess Olmstead <jessolm@cs.washington.edu>
2024-12-01 21:50:47 -0800

    day02 done

---
+++ day02/src/main.rs
@@ -0,0 +1,96 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+fn main() -> std::io::Result<()> {
+    let reports = parse_reports("input/day02.txt")?;
+
+    let part1 = reports.iter().filter(|&report| check_report(report)).count();
+    let part2 = reports.iter().filter(|&report| check_report_part2(report)).count();
+
+    println!("part1: {}", part1);
+    println!("part2: {}", part2);
+
+    Ok(())
+}
+
+fn parse_reports(fname: &str) -> std::io::Result<Vec<Vec<i32>>> {
+    let f = File::open(fname)?;
+    let reader = BufReader::new(f);
+    let lines = reader.lines();
+
+    let mut reports = Vec::new();
+
+    for line in lines {
+        let Ok(line) = line else {
+            continue;
+        };
+        if line.is_empty() {
+            continue;
+        }
+        let report = line.split_whitespace().map(|num| num.parse::<i32>().unwrap()).collect::<Vec<_>>();
+        reports.push(report);
+    }
+
+    Ok(reports)
+}
+
+fn check_report(report: &[i32]) -> bool {
+    let increasing = report[1] > report[0];
+
+    for (i, num) in report.iter().enumerate().skip(1) {
+        if increasing {
+            match num - report[i - 1] {
+                1..=3 => continue,
+                _ => return false,
+            }
+        } else {
+            match num - report[i - 1] {
+                -3..=-1 => continue,
+                _ => return false,
+            }
+        }
+    }
+
+    true
+}
+
+fn check_report_part2(report: &[i32]) -> bool {
+    let mut report_vec = Vec::from(report);
+    if check_report(report) {
+        return true;
+    }
+
+    for (i, num) in report.iter().enumerate() {
+        report_vec.remove(i);
+        if check_report(&report_vec) {
+            return true;
+        } else {
+            report_vec.insert(i, *num);
+        }
+    }
+
+    false
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part1() -> std::io::Result<()> {
+        let reports = parse_reports("../input/day02test.txt")?;
+        let part1 = reports.iter().filter(|&report| check_report(report)).count();
+        assert_eq!(part1, 2);
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_part2() -> std::io::Result<()> {
+        let reports = parse_reports("../input/day02test.txt")?;
+        let part2 = reports.iter().filter(|&report| check_report_part2(report)).count();
+        assert_eq!(part2, 4);
+
+        Ok(())
+    }
+}
