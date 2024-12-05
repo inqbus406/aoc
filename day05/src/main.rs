0ca260c759a846044d8bc769724dbb98bc386bfa
Jess Olmstead <jessolm@cs.washington.edu>
2024-12-04 22:16:43 -0800

    Day05 done

--- day05/src/main.rs
+++ day05/src/main.rs
@@ -31,7 +31,7 @@ fn main() -> std::io::Result<()> {
     }
 
     // dbg!(&rules);
-    let mut part1: usize = pages.iter().filter(|p| is_valid(&p, &rules))
+    let part1: usize = pages.iter().filter(|p| is_valid(&p, &rules))
         .map(|p| p[p.len() / 2]).sum();
     println!("Part 1: {}", part1);
     // for p in pages {
@@ -42,15 +42,39 @@ fn main() -> std::io::Result<()> {
     //     }
     // }
 
+    let part2_pages: Vec<Vec<usize>> = pages.into_iter().filter(|p| !is_valid(&p, &rules)).collect();
+    let mut fixed_pages = Vec::new();
+    for p in part2_pages {
+        let mut fixed = p.clone();
+        while !is_valid(&fixed, &rules) {
+            fix_pages(&mut fixed, &rules);
+        }
+        fixed_pages.push(fixed);
+    }
+    // dbg!(&fixed_pages);
+    let part2: usize = fixed_pages.iter().map(|p| p[p.len() / 2]).sum();
+    println!("Part 2: {}", part2);
+
     Ok(())
+}
+
+fn fix_pages(pages: &mut Vec<usize>, rules: &HashSet<(usize, usize)>) {
+    for combination in pages.clone().iter().combinations(2) {
+        if rules.contains(&(*combination[1], *combination[0])) {
+            // Need to swap these
+            let pos0 = pages.iter().position(|p| p == combination[0]).unwrap();
+            let pos1 = pages.iter().position(|p| p == combination[1]).unwrap();
+            pages.swap(pos0, pos1);
+        }
+    }
 }
 
 fn is_valid(pages: &[usize], rules: &HashSet<(usize, usize)>) -> bool {
 
     // dbg!(&pages);
     let mut combinations = HashSet::new();
-    for perm in pages.iter().combinations(2) {
-        combinations.insert(perm);
+    for combination in pages.iter().combinations(2) {
+        combinations.insert(combination);
     }
     // dbg!(&rules);
     // for perm in &combinations {
@@ -61,5 +85,5 @@ fn is_valid(pages: &[usize], rules: &HashSet<(usize, usize)>) -> bool {
 
     // dbg!(&combinations);
 
-    combinations.iter().all(|perm| !rules.contains(&(*perm[1], *perm[0])))
+    combinations.iter().all(|comb| !rules.contains(&(*comb[1], *comb[0])))
 }
