6ccf343b04bb82aa8f8bd95c6f816df3d405e5ae
Jess Olmstead <jeolmste@amazon.com>
2024-12-06 21:49:07 -0800

    Part2 working (disguised as part1)

--- day07/src/main.rs
+++ day07/src/main.rs
@@ -32,7 +32,14 @@ fn main() -> std::io::Result<()> {
 }
 
 fn is_valid(equation: &Equation) -> bool {
-    if equation.0 == equation.1.iter().sum() || equation.0 == equation.1.iter().product() {
+    if equation.0 == equation.1.iter().sum()
+        || equation.0 == equation.1.iter().product() {
+        return true;
+    }
+    if equation.1.len() < 2 {
+        return false;
+    }
+    if equation.1.len() == 2 && equation.0 == concat(equation.1[0], equation.1[1]) {
         return true;
     }
     if equation.1.len() == 2 {
@@ -40,8 +47,27 @@ fn is_valid(equation: &Equation) -> bool {
     }
     let mut vec1 = vec![equation.1[0] + equation.1[1]];
     let mut vec2 = vec![equation.1[0] * equation.1[1]];
+    let mut vec3 = vec![concat(equation.1[0],equation.1[1])];
     vec1.extend(&equation.1[2..]);
     vec2.extend(&equation.1[2..]);
+    vec3.extend(&equation.1[2..]);
 
-    is_valid(&Equation(equation.0, vec1)) || is_valid(&Equation(equation.0, vec2))
+    is_valid(&Equation(equation.0, vec1))
+        || is_valid(&Equation(equation.0, vec2))
+        || is_valid(&Equation(equation.0, vec3))
+}
+
+fn concat(num1: u64, num2: u64) -> u64 {
+    num1 * 10_u64.pow(num2.to_string().len() as u32) + num2
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_concat() {
+        assert_eq!(concat(11, 12), 1112);
+        assert_eq!(concat(123, 45), 12345);
+    }
 }
