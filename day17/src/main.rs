2bf0ca21a00985b438f204395dfc7de0d9c5d089
Jess Olmstead <jessolm@cs.washington.edu>
2024-12-17 00:44:50 -0800

    Day17 part2 working but needs to be cleaned up

--- day17/src/main.rs
+++ day17/src/main.rs
@@ -3,22 +3,88 @@ use std::io::{BufRead, BufReader};
 
 fn main() -> std::io::Result<()> {
     let mut machine = Machine::from_file("input/day17.txt")?;
-    let outputs = machine.run();
+    let orig_reg_b = machine.reg_b;
+    let orig_reg_c = machine.reg_c;
+    let program = machine.program.clone();
+    let mut outputs = machine.run();
+    print!("Part1: ");
     print!("{}", outputs[0]);
     for output in &outputs[1..] {
         print!(",{}", output);
     }
+    println!();
 
-    // dbg!(&machine);
+    // Part 2:
+
+    // Seems like each output n changes with every 2^(n+1)
+    // 7583068569 was too low
+    // 216133732885152 too low...
+    let mut reg_a: usize = 0;
+    // for (n, output) in program.iter().enumerate().rev() {
+    //     machine.reg_a = reg_a;
+    //     machine.reg_b = orig_reg_b;
+    //     machine.reg_c = orig_reg_c;
+    //     outputs = machine.run();
+    //     while outputs.len() - 1 < n || outputs[n] != *output as usize {
+    //     // while outputs[n] != *output as usize {
+    //         reg_a += 8_usize.pow(n as u32) % 8_usize.pow((n + 1) as u32);
+    //         machine.reg_a = reg_a;
+    //         machine.reg_b = orig_reg_b;
+    //         machine.reg_c = orig_reg_c;
+    //         outputs = machine.run();
+    //     }
+    //     dbg!(&outputs);
+    //     dbg!(&reg_a);
+    // }
+
+    let mut factors = vec![0; program.len()];
+    loop {
+        reg_a = 0;
+        for (i, factor) in factors.iter().enumerate() {
+            reg_a += 8usize.pow(i as u32) * factor;
+        }
+        machine.reg_a = reg_a;
+        machine.reg_b = orig_reg_b;
+        machine.reg_c = orig_reg_c;
+        let outputs = machine.run();
+
+        if outputs.iter().zip(program.iter()).all(|(&num1, &num2)| num1 == num2 as usize) {
+            break;
+        }
+
+        for i in (0..program.len()).rev() {
+            if outputs.len() < i || outputs[i] != program[i] as usize {
+                factors[i] += 1;
+                break;
+            }
+        }
+    }
+
+    // loop {
+    //     reg_a += 1;
+    //     machine.reg_a = reg_a;
+    //     machine.reg_b = orig_reg_b;
+    //     machine.reg_c = orig_reg_c;
+    //     outputs = machine.run();
+    //     if outputs.len() == machine.program.len()
+    //         && outputs.iter().zip(&machine.program).all(|(&num1, &num2)| num1 == num2 as usize) {
+    //         break;
+    //     }
+    //     println!("reg_a: {}", reg_a);
+    //     dbg!(&outputs);
+    // }
+    // dbg!(&outputs);
+
+    println!("Part2: {}", reg_a);
 
     Ok(())
 }
 
 #[derive(Debug)]
 struct Machine {
-    reg_a: i32,
-    reg_b: i32,
-    reg_c: i32,
+    reg_a: usize,
+    reg_b: usize,
+    reg_c: usize,
     program: Vec<u8>,
 }
 
@@ -43,9 +109,9 @@ impl Machine {
             let tokens: Vec<&str> = line.split_whitespace().collect();
             match tokens[0] {
                 "Register" => match tokens[1] {
-                    "A:" => reg_a = tokens.last().unwrap().parse::<i32>().unwrap(),
-                    "B:" => reg_b = tokens.last().unwrap().parse::<i32>().unwrap(),
-                    "C:" => reg_c = tokens.last().unwrap().parse::<i32>().unwrap(),
+                    "A:" => reg_a = tokens.last().unwrap().parse::<usize>().unwrap(),
+                    "B:" => reg_b = tokens.last().unwrap().parse::<usize>().unwrap(),
+                    "C:" => reg_c = tokens.last().unwrap().parse::<usize>().unwrap(),
                     _   => unreachable!(),
                 }
                 "Program:" => {
@@ -58,7 +124,7 @@ impl Machine {
         Ok(Self { reg_a, reg_b, reg_c, program })
     }
 
-    fn run(&mut self) -> Vec::<i32> {
+    fn run(&mut self) -> Vec::<usize> {
         let mut outputs = Vec::new();
         let mut instruction_pointer = 0;
 
@@ -71,13 +137,13 @@ impl Machine {
 
             match instruction {
                 Instruction::Adv => {
-                    self.reg_a = self.reg_a / 2_i32.pow(combo_operand as u32);
+                    self.reg_a = self.reg_a >> combo_operand;
                 },
                 Instruction::Bxl => {
-                    self.reg_b = self.reg_b ^ literal_operand as i32;
+                    self.reg_b = self.reg_b ^ literal_operand as usize;
                 },
                 Instruction::Bst => {
-                    self.reg_b = combo_operand % 8;
+                    self.reg_b = (combo_operand % 8) as usize;
                 },
                 Instruction::Jnz => {
                     if self.reg_a != 0 {
@@ -93,10 +159,10 @@ impl Machine {
                     outputs.push(result);
                 },
                 Instruction::Bdv => {
-                    self.reg_b = self.reg_a / 2_i32.pow(combo_operand as u32);
+                    self.reg_b = self.reg_a >> combo_operand;
                 },
                 Instruction::Cdv => {
-                    self.reg_c = self.reg_a / 2_i32.pow(combo_operand as u32);
+                    self.reg_c = self.reg_a >> combo_operand;
                 },
             }
 
@@ -106,9 +172,9 @@ impl Machine {
         outputs
     }
 
-    fn combo_operand(&self, num: u8) -> i32 {
+    fn combo_operand(&self, num: u8) -> usize {
         match num {
-            x @ 0..=3 => x as i32,
+            x @ 0..=3 => x as usize,
             4 => self.reg_a,
             5 => self.reg_b,
             6 => self.reg_c,
