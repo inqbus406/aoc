cbe01177a445d06f5be137b69794570f5d155372
Jess Olmstead <jeolmste@amazon.com>
2024-12-19 11:16:49 -0800

    Add unit tests and timing for day19

--- day19/src/main.rs
+++ day19/src/main.rs
@@ -1,12 +1,36 @@
 use std::collections::{HashMap, HashSet};
 use std::fs::File;
 use std::io::{BufRead, BufReader};
+use std::time::Instant;
 
 fn main() -> std::io::Result<()> {
     let f = File::open("input/day19.txt")?;
     let reader = BufReader::new(f);
-    let mut lines = reader.lines();
 
+    let (towels, patterns) = get_towels_and_patterns(reader);
+
+    let start = Instant::now();
+    let part1 = patterns.iter()
+        .filter(|p| is_possible(p, &towels))
+        .count();
+    let part1_end = Instant::now();
+    println!("Part1: {}, duration: {:?}", part1, part1_end.duration_since(start));
+
+    let part2_start = Instant::now();
+    let mut cached_searcher = Searcher::new();
+
+    let part2 = patterns.iter()
+        .map(|p| cached_searcher.possibilities(p, &towels))
+        .sum::<usize>();
+    let part2_end = Instant::now();
+    println!("Part2: {}, duration: {:?}", part2, part2_end.duration_since(part2_start));
+
+
+    Ok(())
+}
+
+fn get_towels_and_patterns(reader: BufReader<File>) -> (Vec<String>, Vec<String>) {
+    let mut lines = reader.lines();
     let towels = lines.next().unwrap().unwrap()
         .split(", ")
         .map(|s| String::from(s))
@@ -25,20 +49,7 @@ fn main() -> std::io::Result<()> {
         patterns.push(line);
     }
 
-    let part1 = patterns.iter()
-        .filter(|p| is_possible(p, &towels))
-        .count();
-    println!("Part1: {}", part1);
-
-    let mut cached_searcher = Searcher::new();
-
-    let part2 = patterns.iter()
-        .map(|p| cached_searcher.possibilities(p, &towels))
-        .sum::<usize>();
-    println!("Part2: {}", part2);
-
-
-    Ok(())
+    (towels, patterns)
 }
 
 fn is_possible(pattern: &str, towels: &Vec<String>) -> bool {
@@ -58,28 +69,6 @@ fn is_possible(pattern: &str, towels: &Vec<String>) -> bool {
     }
 
     false
-}
-
-fn possibilities(pattern: &str, towels: &Vec<String>) -> usize {
-    let matching_towels = towels.iter()
-        .filter(|s| s.len() == pattern.len())
-        .filter(|s| s == &pattern)
-        .collect::<HashSet<_>>();
-    let mut count = matching_towels.len();
-    let remaining_towels = towels.iter()
-        .filter(|s| !matching_towels.contains(s))
-        .collect::<Vec<_>>();
-    for towel in remaining_towels {
-        match pattern.find(towel) {
-            Some(0) => {
-                let substring = &pattern[towel.chars().count()..];
-                count += possibilities(substring, towels);
-            },
-            _ => continue,
-        }
-    }
-
-    count
 }
 
 struct Searcher<'a> {
@@ -118,5 +107,40 @@ impl<'a> Searcher<'a> {
         self.cache.insert(pattern, count);
 
         count
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part1() -> std::io::Result<()> {
+        let f = File::open("../test_input/day19test.txt")?;
+        let reader = BufReader::new(f);
+        let (towels, patterns) = get_towels_and_patterns(reader);
+
+        let part1 = patterns.iter()
+            .filter(|p| is_possible(p, &towels))
+            .count();
+       assert_eq!(part1, 6);
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_part2() -> std::io::Result<()> {
+        let f = File::open("../test_input/day19test.txt")?;
+        let reader = BufReader::new(f);
+        let (towels, patterns) = get_towels_and_patterns(reader);
+
+        let mut cached_searcher = Searcher::new();
+
+        let part2 = patterns.iter()
+            .map(|p| cached_searcher.possibilities(p, &towels))
+            .sum::<usize>();
+        assert_eq!(part2, 16);
+
+        Ok(())
     }
 }
