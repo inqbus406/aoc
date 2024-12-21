8639c91840b0543c2b9b17021ab6f06246c571f7
Jess Olmstead <jessolm@cs.washington.edu>
2024-12-21 11:27:34 -0800

    Day21 part 1 working

--- day21/src/main.rs
+++ day21/src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::VecDeque;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
@@ -80,6 +81,36 @@ impl NumericKey {
             NumericKey::Nine => (2, 0),
         }
     }
+
+    fn from_position(pos: (i32, i32)) -> Option<Self> {
+        match pos {
+            (2, 3) => Some(NumericKey::A),
+            (1, 3) => Some(NumericKey::Zero),
+            (0, 2) => Some(NumericKey::One),
+            (1, 2) => Some(NumericKey::Two),
+            (2, 2) => Some(NumericKey::Three),
+            (0, 1) => Some(NumericKey::Four),
+            (1, 1) => Some(NumericKey::Five),
+            (2, 1) => Some(NumericKey::Six),
+            (0, 0) => Some(NumericKey::Seven),
+            (1, 0) => Some(NumericKey::Eight),
+            (2, 0) => Some(NumericKey::Nine),
+            _ => None,
+        }
+    }
+
+    fn next(&self, dir: &TClusterKey) -> Option<Self> {
+        let x = self.position().0 as i32;
+        let y = self.position().1 as i32;
+        match dir {
+            TClusterKey::Down => Self::from_position((x, y + 1)),
+            TClusterKey::Up => Self::from_position((x, y - 1)),
+            TClusterKey::Left => Self::from_position((x - 1, y)),
+            TClusterKey::Right => Self::from_position((x + 1, y)),
+            _ => None,
+        }
+    }
+
 }
 
 #[derive(Debug, Clone)]
@@ -111,6 +142,13 @@ impl TClusterKey {
             TClusterKey::Up => '^',
         }
     }
+
+}
+
+#[derive(Debug, Clone)]
+struct Next {
+    pos: NumericKey,
+    path: Vec<TClusterKey>
 }
 
 struct NumericKeypadRobot {
@@ -150,106 +188,61 @@ impl NumericKeypadRobot {
         }
         self.position = digit.clone();
 
+        // println!("Moving from {:?} to {:?}: {:?}", self.position, digit, &result);
+
         result
     }
 
     fn moves_to_digit(&self, digit: &NumericKey) -> Vec<Vec<TClusterKey>> {
-        // Always move right before down and up before left to avoid the gap
-        let start = self.position.position();
+        // let start = self.position.position();
         let end = digit.position();
 
         let mut result = Vec::new();
 
-        let mut path0 = Vec::new();
-        let mut temp = start.clone();
-        while temp != end {
+        let mut fringe = VecDeque::new();
+        fringe.push_back(Next { pos: self.position.clone(), path: Vec::new() });
 
-            if temp.1 > end.1 {
-                path0.push(TClusterKey::Up);
-                temp.1 -= 1;
+        while let Some(current) = fringe.pop_front() {
+            // dbg!(&current);
+            if current.pos.position() == end {
+                let mut good_path = current.path.clone();
+                good_path.push(TClusterKey::A);
+                result.push(good_path);
             }
 
-            if temp.0 > end.0 {
-                path0.push(TClusterKey::Left);
-                temp.0 -= 1;
-            } else if temp.0 < end.0 {
-                path0.push(TClusterKey::Right);
-                temp.0 += 1;
-            }
-
-            if temp.1 < end.1 {
-                path0.push(TClusterKey::Down);
-                temp.1 += 1;
+            for next in Self::get_moves_toward(&current.pos, digit) {
+                // dbg!(&next);
+                let mut path = current.path.clone();
+                path.push(next.clone());
+                fringe.push_back(Next { pos: current.pos.next(&next).unwrap(), path });
             }
         }
-        path0.push(TClusterKey::A);
-        result.push(path0);
-
-        let mut path1 = Vec::new();
-
-        let moving_up = start.1 > end.1;
-        let moving_left = start.0 > end.0;
-
-        if moving_up {
-            for _ in 0..start.1.abs_diff(end.1) {
-                path1.push(TClusterKey::Up);
-            }
-        }
-
-        if moving_left {
-            for _ in 0..start.0.abs_diff(end.0) {
-                path1.push(TClusterKey::Left);
-            }
-        } else {
-            for _ in 0..start.0.abs_diff(end.0) {
-                path1.push(TClusterKey::Right);
-            }
-        }
-
-        if !moving_up {
-            for _ in 0..start.1.abs_diff(end.1) {
-                path1.push(TClusterKey::Down);
-            }
-        }
-
-        // Always add an A at the end
-        path1.push(TClusterKey::A);
-        result.push(path1);
-
-        let mut path2 = Vec::new();
-
-        let moving_up = start.1 > end.1;
-        let moving_left = start.0 > end.0;
-
-        if moving_left {
-            for _ in 0..start.0.abs_diff(end.0) {
-                path2.push(TClusterKey::Left);
-            }
-        } else {
-            for _ in 0..start.0.abs_diff(end.0) {
-                path2.push(TClusterKey::Right);
-            }
-        }
-        if moving_up {
-            for _ in 0..start.1.abs_diff(end.1) {
-                path2.push(TClusterKey::Up);
-            }
-        }
-
-        if !moving_up {
-            for _ in 0..start.1.abs_diff(end.1) {
-                path2.push(TClusterKey::Down);
-            }
-        }
-
-        // Always add an A at the end
-        if start.1 != 3 {
-            path2.push(TClusterKey::A);
-            result.push(path2);
-        }
+        // println!("Found valid paths from {:?} to {:?}", self.position, digit);
+        // dbg!(&result);
 
         result
     }
+
+    fn get_moves_toward(start: &NumericKey, end: &NumericKey) -> Vec<TClusterKey> {
+        let result = [TClusterKey::Up, TClusterKey::Down, TClusterKey::Left, TClusterKey::Right].into_iter()
+            .filter(|pos| start.next(&pos).is_some())
+            .filter(|pos| start.next(&pos).unwrap().position() != (0, 3))  // Don't step in the gap
+            .filter(|pos| {
+                let test = Self::manhattan_distance(&start.next(&pos).unwrap(), end) < Self::manhattan_distance(start, end);
+                // println!("{:?} valid move from {:?} toward {:?}? {test}", &start.next(&pos).unwrap(), start, end);
+                test
+            })
+            .collect();
+
+        // println!("Going from {:?} to {:?}", start, end);
+        // dbg!(&result);
+        result
+    }
+
+    fn manhattan_distance(start: &NumericKey, end: &NumericKey) -> usize {
+        start.position().0.abs_diff(end.position().0) + start.position().1.abs_diff(end.position().1)
+    }
+
 }
 
 impl TClusterKeypadRobot1 {
@@ -361,6 +354,18 @@ impl TClusterKeypadRobot2 {
 
         result.push(TClusterKey::A);
         result
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_manhattan_distance() {
+        assert_eq!(NumericKeypadRobot::manhattan_distance(&NumericKey::A, &NumericKey::Zero), 1);
+        assert_eq!(NumericKeypadRobot::manhattan_distance(&NumericKey::A, &NumericKey::One), 3);
+        assert_eq!(NumericKeypadRobot::manhattan_distance(&NumericKey::A, &NumericKey::Seven), 5);
     }
 }
 
