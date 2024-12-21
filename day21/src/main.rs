d4a416c686791ad47855575e9a4c286cb98f023b
Jess Olmstead <jessolm@cs.washington.edu>
2024-12-21 10:26:45 -0800

    Working on day21part1 test input, but ugly. Doesn't work on full input

--- day21/src/main.rs
+++ day21/src/main.rs
@@ -2,9 +2,11 @@ use std::fs::File;
 use std::io::{BufRead, BufReader};
 
 fn main() -> std::io::Result<()> {
-    let f = File::open("test_input/day21test.txt")?;
+    let f = File::open("input/day21.txt")?;
     let reader = BufReader::new(f);
     let lines = reader.lines();
+
+    let mut part1_sum = 0;
 
     for line in lines {
         let Ok(code) = line else {
@@ -19,8 +21,12 @@ fn main() -> std::io::Result<()> {
             result.push_str(&keypad_robot.enter_digit(&NumericKey::from_char(c)));
             // result.push_str(&format!("   making {}: {}      ", c, keypad_robot.enter_digit(&NumericKey::from_char(c))));
         }
-        println!("{}: {}, len: {}", code, result, result.len());
+        let num = code.split('A').take(1).map(|s| s.parse::<usize>().unwrap()).collect::<Vec<_>>();
+        part1_sum += num[0] * result.len();
+        // println!("{}: {}, len: {}", code, result, result.len());
     }
+
+    println!("Part1: {part1_sum}");
 
 
     Ok(())
@@ -132,72 +138,115 @@ impl NumericKeypadRobot {
     fn enter_digit(&mut self, digit: &NumericKey) -> String {
         // println!("Keypad robot wants {:?}", digit);
         let mut result = String::new();
-        for key in self.moves_to_digit(digit) {
-            // println!("Keypad robot asks for {:?}", key);
-            result.push_str(&self.robot.enter_direction(&key));
+        for path in self.moves_to_digit(digit) {
+            let mut temp = String::new();
+            for key in path {
+                // println!("Keypad robot asks for {:?}", key);
+                temp.push_str(&self.robot.enter_direction(&key));
+            }
+            if result.is_empty() || temp.len() < result.len() {
+                result = temp;
+            }
         }
         self.position = digit.clone();
 
         result
     }
 
-    fn moves_to_digit(&self, digit: &NumericKey) -> Vec<TClusterKey> {
+    fn moves_to_digit(&self, digit: &NumericKey) -> Vec<Vec<TClusterKey>> {
         // Always move right before down and up before left to avoid the gap
         let start = self.position.position();
         let end = digit.position();
 
         let mut result = Vec::new();
-        // let mut temp = start.clone();
-        //
-        // while temp != end {
-        //     // println!("Hanging here, temp: {:?}, end: {:?}", &temp, &end);
-        //
-        //     if temp.1 > end.1 {
-        //         result.push(TClusterKey::Up);
-        //         temp.1 -= 1;
-        //     }
-        //
-        //     if temp.0 > end.0 {
-        //         result.push(TClusterKey::Left);
-        //         temp.0 -= 1;
-        //     } else if temp.0 < end.0 {
-        //         result.push(TClusterKey::Right);
-        //         temp.0 += 1;
-        //     }
-        //
-        //     if temp.1 < end.1 {
-        //         result.push(TClusterKey::Down);
-        //         temp.1 += 1;
-        //     }
-        // }
+
+        let mut path0 = Vec::new();
+        let mut temp = start.clone();
+        while temp != end {
+
+            if temp.1 > end.1 {
+                path0.push(TClusterKey::Up);
+                temp.1 -= 1;
+            }
+
+            if temp.0 > end.0 {
+                path0.push(TClusterKey::Left);
+                temp.0 -= 1;
+            } else if temp.0 < end.0 {
+                path0.push(TClusterKey::Right);
+                temp.0 += 1;
+            }
+
+            if temp.1 < end.1 {
+                path0.push(TClusterKey::Down);
+                temp.1 += 1;
+            }
+        }
+        path0.push(TClusterKey::A);
+        result.push(path0);
+
+        let mut path1 = Vec::new();
 
         let moving_up = start.1 > end.1;
         let moving_left = start.0 > end.0;
 
         if moving_up {
             for _ in 0..start.1.abs_diff(end.1) {
-                result.push(TClusterKey::Up);
+                path1.push(TClusterKey::Up);
             }
         }
 
         if moving_left {
             for _ in 0..start.0.abs_diff(end.0) {
-                result.push(TClusterKey::Left);
+                path1.push(TClusterKey::Left);
             }
         } else {
             for _ in 0..start.0.abs_diff(end.0) {
-                result.push(TClusterKey::Right);
+                path1.push(TClusterKey::Right);
             }
         }
 
         if !moving_up {
             for _ in 0..start.1.abs_diff(end.1) {
-                result.push(TClusterKey::Down);
+                path1.push(TClusterKey::Down);
             }
         }
 
         // Always add an A at the end
-        result.push(TClusterKey::A);
+        path1.push(TClusterKey::A);
+        result.push(path1);
+
+        let mut path2 = Vec::new();
+
+        let moving_up = start.1 > end.1;
+        let moving_left = start.0 > end.0;
+
+        if moving_left {
+            for _ in 0..start.0.abs_diff(end.0) {
+                path2.push(TClusterKey::Left);
+            }
+        } else {
+            for _ in 0..start.0.abs_diff(end.0) {
+                path2.push(TClusterKey::Right);
+            }
+        }
+        if moving_up {
+            for _ in 0..start.1.abs_diff(end.1) {
+                path2.push(TClusterKey::Up);
+            }
+        }
+
+        if !moving_up {
+            for _ in 0..start.1.abs_diff(end.1) {
+                path2.push(TClusterKey::Down);
+            }
+        }
+
+        // Always add an A at the end
+        if start.1 != 3 {
+            path2.push(TClusterKey::A);
+            result.push(path2);
+        }
 
         result
     }
